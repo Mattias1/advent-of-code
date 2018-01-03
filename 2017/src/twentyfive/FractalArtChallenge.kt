9edf29b586e2d27af4a6588291f5c5347530f533
Mattias Beimers <mattias.beimers@topicus.nl>
2018-01-03 16:45:59 +0100

    Fix the fractal art challenge

--- 2017/src/twentyfive/FractalArtChallenge.kt
+++ 2017/src/twentyfive/FractalArtChallenge.kt
@@ -5,8 +5,10 @@ import gridList.*
 class FractalArtChallenge {
     fun pixelAmount(input: List<String>, iterationAmount: Int = 5): Int {
         val rules = getRules(input)
-        var grid = ruleToGrid(".#./..#/###")
-        return 0
+        val initialGrid: GridList<Boolean> = ruleToGrid(".#./..#/###")
+
+        val grid = (1..iterationAmount).fold(initialGrid, { g, _ -> step(g, rules) })
+        return grid.values.count { it }
     }
 
     private fun getRules(input: List<String>): Map<String, String> =
@@ -17,13 +19,6 @@ class FractalArtChallenge {
 
     private fun step(grid: GridList<Boolean>, rules: Map<String, String>): GridList<Boolean> {
         val size = if (grid.width % 2 == 0) 2 else 3
-        // val converted = grid.splitToSubGrids(size, size).map { convert(it, rules) }
-        // return gridOf(converted)
-        return grid
-    }
-
-    private fun convert(grid: GridList<Boolean>, rules: Map<String, String>): GridList<Boolean> {
-        val size = if (grid.width % 2 == 0) 2 else 3
         val subGrids = grid.splitToSubGrids(size, size)
         val convertedSubGrids = subGrids.map { g ->
             val ruleKey = rules.keys.first { belongsToGroup(g, it) }
@@ -32,12 +27,11 @@ class FractalArtChallenge {
         return joinGrids(convertedSubGrids)
     }
 
-    private fun belongsToGroup(grid: GridList<Boolean>, rule: String): Boolean {
+    fun belongsToGroup(grid: GridList<Boolean>, rule: String): Boolean {
         val rotatedLeft = grid.rotateLeft()
         val flip = grid.reflectVertical()
         val flipRotatedLeft = flip.rotateLeft()
         val d4 = listOf(
-                // TODO: Is this all? Is this too much? Is this the group called D4?
                 grid, rotatedLeft, rotatedLeft.rotateLeft(), grid.rotateRight(),
                 flip, flipRotatedLeft, flipRotatedLeft.rotateLeft(), flip.rotateRight()
         )
@@ -49,7 +43,7 @@ class FractalArtChallenge {
                 .map { if (it) '#' else '.' }
                 .rows
                 .map { it.joinToString("") }
-                .joinToString("")
+                .joinToString("/")
         return stringRepresentation == rule
     }
 }
--- 2017/src/twentyfive/FractalArtChallengeTest.kt
+++ 2017/src/twentyfive/FractalArtChallengeTest.kt
@@ -1,5 +1,6 @@
 package twentyfive
 
+import gridList.gridOf
 import junit.framework.TestCase
 import org.junit.Assert
 
@@ -13,5 +14,23 @@ class FractalArtChallengeTest : TestCase() {
         )
         val result = challenge.pixelAmount(input, 2)
         Assert.assertEquals(12, result)
+    }
+
+    fun testBelongsToGroup_identity() {
+        val grid = gridOf(false, false, false, true)
+        val rule = "../.#"
+
+        val result = challenge.belongsToGroup(grid, rule)
+
+        Assert.assertTrue(result)
+    }
+
+    fun testBelongsToGroup_rotation() {
+        val grid = gridOf(false, false, true, false)
+        val rule = "../.#"
+
+        val result = challenge.belongsToGroup(grid, rule)
+
+        Assert.assertTrue(result)
     }
 }
